a7318ae970531b1e4c29f8ab35dbe3c52ef9d492
Emily <emilywdeng@gmail.com>
2018-06-25 22:42:53 -0400

    installed card swipe package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Cards, { Card } from 'react-swipe-card';
+import './react-swipe-card.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello world!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const data = ['Alexandre', 'Thomas', 'Lucien'];
+
+const CustomAlertLeft = () => <span>Nop</span>;
+const CustomAlertRight = () => <span>Ok</span>;
+
+const App = () => {
+  return (
+    <div className="App">
+  <Cards onEnd={console.log("action('end')")} className='master-root'>
+      {data.map(item =>
+        <Card
+          key={item}
+          onSwipeLeft={() => {console.log("action('swipe left')")}}
+          onSwipeRight={() => {console.log("action('swipe right')")}}>
+          <h2>{item}</h2>
+        </Card>
+      )}
+    </Cards>
+    </div>
+  );
+};
 
 export default App;
-
-
